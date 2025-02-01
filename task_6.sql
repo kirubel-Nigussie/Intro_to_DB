a72e4caeafadfaaac4bc34082061319d5b8a1fa3
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-02-01 10:11:57 +0300

    2nd commit

--- task_6.sql
+++ task_6.sql
@@ -1,11 +1,1 @@
 
-SHOW DATABASES;
-USE alx_book_store;
-
-SHOW TABLES;
-DESC customers;
-INSERT INTO customers(customer_id, customer_name, email, address  ) 
-VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave." );
-
-
-
